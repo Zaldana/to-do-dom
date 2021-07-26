f0ba33e425c8d4d7b929c690210de0654000fc7e
Zaldana <rooben.zaldana@gmail.com>
2021-07-26 12:20:12 -0400

    fix edit option

--- main.js
+++ main.js
@@ -20,7 +20,6 @@ addButton.on('click', function () {
         'width': '80%',
         'padding-top': '9px',
         'background-color': 'white'
-
     });
 
     newTaskdiv.css({
@@ -42,50 +41,61 @@ addButton.on('click', function () {
 });
 
 removeButton.on('click', function () {
+    
 
     $('body').find('.task:checked').closest('.boxes').remove();
 
 });
 
 editButton.on('click', function () {
-    
-    $('body').find('.task:checked').closest('.boxes').remove();
-    const box = $("<div class='boxes'></div>");
+   
     const newTasktext = inputText.val();
-    const newTaskdiv = $("<input type='checkbox' name='task-checkbox' class='task'>");
-    const label = $("<label for='task-checkbox' class='label'></label>").text(newTasktext);
 
-    box.css({
-        'height': '50px',
-        'display': 'flex',
-        'align-items': 'center',
-        'border': '1px solid pink',
-        'margin-left': '40px',
-        'width': '80%',
-        'padding-top': '9px',
-        'background-color': 'white'
-
-    });
-
-    newTaskdiv.css({
-        'height': '30px',
-        'line-height': '30px',
-        'width': '30px',
-        'margin-left': '20px',
-    });
-
-    label.css({
-        'font-size': '20px',
-        'font-family': "'Montserrat', sans-serif",
-        'color': '#3ed1c2',
-        'padding-bottom': '8px'
-    });
-
-    $('body').append(box);
-    $(box).append(newTaskdiv, label);
     
-    
+    const replaceLabel = $('body').
+                            find('.task:checked').
+                                closest('.boxes').
+                                    find('.label').
+                                        text(newTasktext);
+
 });
+
+//     const box = $("<div class='boxes'></div>");
+//     const newTasktext = inputText.val();
+//     const newTaskdiv = $("<input type='checkbox' name='task-checkbox' class='task'>");
+//     const label = $("<label for='task-checkbox' class='label'></label>").text(newTasktext);
+
+//     box.css({
+//         'height': '50px',
+//         'display': 'flex',
+//         'align-items': 'center',
+//         'border': '1px solid pink',
+//         'margin-left': '40px',
+//         'width': '80%',
+//         'padding-top': '9px',
+//         'background-color': 'white'
+
+//     });
+
+//     newTaskdiv.css({
+//         'height': '30px',
+//         'line-height': '30px',
+//         'width': '30px',
+//         'margin-left': '20px',
+//     });
+
+//     label.css({
+//         'font-size': '20px',
+//         'font-family': "'Montserrat', sans-serif",
+//         'color': '#3ed1c2',
+//         'padding-bottom': '8px'
+//     });
+
+//     $('body').append(box);
+//     $(box).append(newTaskdiv, label);
+    
+    
+// });
 
 
 removeAllbutton.on('click', function () {
